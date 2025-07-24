c601d0f9d6499f70432799a88e75129e71f5473d
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-07-23 21:52:25 -0300

    initial commit

---
+++ src/main/java/br/com/geisonbrunodev/jwtauthappbackend/JwtauthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.geisonbrunodev.jwtauthappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtauthAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/geisonbrunodev/jwtauthappbackend/JwtauthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geisonbrunodev.jwtauthappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtauthAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
