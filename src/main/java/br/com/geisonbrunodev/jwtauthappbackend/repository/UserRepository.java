c2c9b76a87dd25c2f1895306277bbbc92f53186a
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-08-07 20:45:57 -0300

    feat: add UserRepository interface for user data access

---
+++ src/main/java/br/com/geisonbrunodev/jwtauthappbackend/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.geisonbrunodev.jwtauthappbackend.repository;
+
+import br.com.geisonbrunodev.jwtauthappbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    User save(User user);
+}
