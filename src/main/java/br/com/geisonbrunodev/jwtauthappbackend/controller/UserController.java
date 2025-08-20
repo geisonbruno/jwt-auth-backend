538ecee0a3c3b09a50129c775f1146e6fee6547b
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-08-20 20:53:32 -0300

    feat(api): add user profile endpoint to UserController

--- src/main/java/br/com/geisonbrunodev/jwtauthappbackend/controller/UserController.java
+++ src/main/java/br/com/geisonbrunodev/jwtauthappbackend/controller/UserController.java
@@ -2,9 +2,13 @@ package br.com.geisonbrunodev.jwtauthappbackend.controller;
 
 import br.com.geisonbrunodev.jwtauthappbackend.dto.LoginDTO;
 import br.com.geisonbrunodev.jwtauthappbackend.dto.TokenDTO;
+import br.com.geisonbrunodev.jwtauthappbackend.dto.UserDTO;
 import br.com.geisonbrunodev.jwtauthappbackend.dto.UserRegisterDTO;
+import br.com.geisonbrunodev.jwtauthappbackend.entity.User;
 import br.com.geisonbrunodev.jwtauthappbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +27,13 @@ public class UserController {
     public TokenDTO login(@RequestBody LoginDTO loginDTO) {
         return userService.authenticate(loginDTO);
     }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserDTO> getProfile(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        UserDTO dto = new UserDTO(user.getId(), user.getName(), user.getUsername(), user.getEmail(), user.getRole());
+        return ResponseEntity.ok(dto);
+    }
+
 
 }
