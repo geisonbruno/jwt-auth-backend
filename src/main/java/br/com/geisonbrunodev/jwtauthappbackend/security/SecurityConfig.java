232fd2728c7228c1d533c73b53cbc0610994a24c
Geison Bruno Dos Santos <110945121+geisonbruno@users.noreply.github.com>
2025-08-20 21:14:19 -0300

    feat: add JWT filter and stateless session to security config

--- src/main/java/br/com/geisonbrunodev/jwtauthappbackend/security/SecurityConfig.java
+++ src/main/java/br/com/geisonbrunodev/jwtauthappbackend/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.geisonbrunodev.jwtauthappbackend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -16,13 +21,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthFilter) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/auth/**").permitAll()
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/auth/**", "/users/register", "/users/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
-
 }
